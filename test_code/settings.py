228bbc13ba0d4032a99ebb203fa895b2bd0a96a3
FFiuca <fardana.fiuca31@gmail.com>
2024-08-29 06:55:12 +0700

    fixing cors

--- test_code/settings.py
+++ test_code/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "django_extensions",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,9 +129,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS_ALLOWED_ORIGINS = [
-#     '*'
-# ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 REST_FRAMEWORK  = {
