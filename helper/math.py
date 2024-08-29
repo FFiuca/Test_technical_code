0dc65a98bf3dad24c23f3fe6944371cd1a4a8f55
FFiuca <fardana.fiuca31@gmail.com>
2024-08-30 06:10:01 +0700

    cp

--- helper/math.py
+++ helper/math.py
@@ -1,3 +1,6 @@
+# This function checks if a given integer value is a prime number.
+# It returns True if the number is prime and False otherwise.
+# Note: The function currently does not handle cases for numbers less than 2 correctly.
 def is_prime(val):
     is_prime = False if val==2 else True
     for x in range(2, val):
